8ea22101f1e6f2d83436d404033261331d6576a7
Justyn <egot@istic.pw>
2018-02-03 10:41:30 -0600

    Use jua.go

--- example.lua
+++ example.lua
@@ -26,34 +26,37 @@ jua.on("mouse_click", function(event, button, x, y)
   print("Mouse Click at X: "..x.." Y: "..y)
 end)
 
-w.open({
-  success = function(url, handle)
-    ws = handle
-    print("Successfully connected.")
-    ws.send("Hello world!")
+jua.go(function()
+  w.open({
+    success = function(url, handle)
+      ws = handle
+      print("Successfully connected.")
+      ws.send("Hello world!")
 
-    jua.setInterval(function()
-      timePassed = timePassed + 1
-      ws.send(timePassed.." seconds have passed.")
-    end, 1)
+      jua.setInterval(function()
+        timePassed = timePassed + 1
+        ws.send(timePassed.." seconds have passed.")
+      end, 1)
 
-    jua.setTimeout(function()
-      print("The program ran for 15 seconds. Exiting...")
-      ws.close()
+      jua.setTimeout(function()
+        print("The program ran for 15 seconds. Exiting...")
+        ws.close()
+        jua.stop()
+      end, 15)
+    end,
+    
+    failure = function(url)
+      print("Failed to connect.")
       jua.stop()
-    end, 15)
-  end,
-  failure = function(url)
-    print("Failed to connect.")
-    jua.stop()
-  end,
-  message = function(url, data)
-    print("Message: "..data)
-  end,
-  closed = function(url)
-    print("Connection closed")
-    jua.stop()
-  end
-}, "ws://echo.websocket.org")
-
-jua.run()
+    end,
+    
+    message = function(url, data)
+      print("Message: "..data)
+    end,
+    
+    closed = function(url)
+      print("Connection closed")
+      jua.stop()
+    end
+  }, "ws://echo.websocket.org")
+end)
